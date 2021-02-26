e80adc34ddb5a12ff7fb0cebe2c7ea9b14a541bf
Leandro Silva Luz do Carmo <leandro_scarmo@hotmail.com>
2021-02-26 17:11:33 -0300

    feat: Create user with account

--- lib/rocketpay_web/views/error_view.ex
+++ lib/rocketpay_web/views/error_view.ex
@@ -1,16 +1,25 @@
 defmodule RocketpayWeb.ErrorView do
   use RocketpayWeb, :view
 
-  # If you want to customize a particular status code
-  # for a certain format, you may uncomment below.
-  # def render("500.json", _assigns) do
-  #   %{errors: %{detail: "Internal Server Error"}}
-  # end
+  alias RocketpayWeb.ErrorHelpers
+  alias Ecto.Changeset
 
-  # By default, Phoenix returns the status message from
-  # the template name. For example, "404.json" becomes
-  # "Not Found".
+  def render("400.json" = template, %{result: %Changeset{} = changeset}) do
+    %{errors:
+      %{
+        detail: Phoenix.Controller.status_message_from_template(template),
+        message: get_errors(changeset)
+      }
+    }
+  end
+
   def template_not_found(template, _assigns) do
     %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
+  end
+
+  defp get_errors(changeset) do
+    Changeset.traverse_errors(changeset, fn {msg, opts} ->
+      ErrorHelpers.translate_error({msg, opts})
+    end)
   end
 end
