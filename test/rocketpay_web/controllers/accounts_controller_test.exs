4615a2e8a9c0d5814f855c923fd32896ec1a91ce
Leandro Silva Luz do Carmo <leandrosluzcarmo@gmail.com>
2021-04-07 23:26:55 +0000

    test: Account withdraw tests

--- test/rocketpay_web/controllers/accounts_controller_test.exs
+++ test/rocketpay_web/controllers/accounts_controller_test.exs
@@ -6,20 +6,9 @@ defmodule RocketpayWeb.AccountsControllerTest do
 
   describe "deposit/2" do
     setup %{conn: conn} do
-      params = %{
-        name: "Test User",
-        age: 20,
-        email: "test-user@example.com",
-        password: "Pass$123",
-        nickname: "test-user"
-      }
+      {:ok, %{account_id: account_id, params: params}} = create_user("Test User", "test-user")
 
-      {:ok, %User{account: %Account{id: account_id}}} = Rocketpay.create_user(params)
-
-      {:ok, token} = TestHelper.authenticate(params)
-
-      conn = conn
-        |> put_req_header("authorization", "Bearer " <> token)
+      conn = conn |> authenticate(params)
 
       {:ok, conn: conn, account_id: account_id}
     end
@@ -48,5 +37,67 @@ defmodule RocketpayWeb.AccountsControllerTest do
 
         assert %{"errors" => %{"message" => "Invalid deposit value"}} = response
     end
+  end
+
+  describe "withdraw/2" do
+    setup %{conn: conn} do
+      {:ok, %{user: user, account_id: account_id, params: params}} = create_user("Test User", "test-user")
+
+      conn = conn |> authenticate(params)
+
+      {:ok, conn: conn, user: user, account_id: account_id}
+    end
+
+    test "make withdraw", %{conn: conn, user: user, account_id: account_id} do
+      Rocketpay.deposit(%{"id" => account_id, "value" => "50.0"}, user)
+
+      params = %{"value" => "50.0"}
+
+      response =
+        conn
+        |> put(Routes.accounts_path(conn, :withdraw, account_id, params))
+        |> json_response(:ok)
+
+      assert %{
+        "account" => %{"balance" => "0.0", "id" => _id},
+        "message" => "Balance updated"
+      } = response
+    end
+
+    test "returns an error if the withdraw value is greather than the account amount", %{conn: conn, user: user, account_id: account_id} do
+      Rocketpay.deposit(%{"id" => account_id, "value" => "30.0"}, user)
+
+      params = %{"value" => "50.0"}
+
+      response =
+        conn
+        |> put(Routes.accounts_path(conn, :withdraw, account_id, params))
+        |> json_response(:bad_request)
+
+      assert %{"errors" => %{"message" => %{"balance" => ["is invalid"]}}} = response
+    end
+
+    test "returns an error when there are invalid params", %{conn: conn, account_id: account_id} do
+      params = %{"value" => "abc"}
+
+      response =
+        conn
+        |> put(Routes.accounts_path(conn, :withdraw, account_id, params))
+        |> json_response(:bad_request)
+
+      assert %{"errors" => %{"message" => "Invalid withdraw value"}} = response
+    end
+  end
+
+  defp create_user(name, nickname) do
+    {:ok, %User{account: %Account{id: account_id}} = user, params} = TestHelper.create_user(name, nickname)
+
+    {:ok, %{user: user, account_id: account_id, params: params}}
+  end
+
+  defp authenticate(conn, params) do
+    {:ok, token} = TestHelper.authenticate(params)
+
+    conn |> put_req_header("authorization", "Bearer " <> token)
   end
 end
--- test/support/test_helper.ex
+++ test/support/test_helper.ex
@@ -2,6 +2,20 @@ defmodule RocketpayWeb.TestHelper do
   alias Rocketpay.UserManager.Manager, as: UserManager
   alias Rocketpay.UserManager.Guardian
 
+  def create_user(name, nickname) do
+    params = %{
+      name: name,
+      age: 20,
+      email: "#{nickname}@example.com",
+      password: "Pass$123",
+      nickname: nickname
+    }
+
+    {:ok, user} = Rocketpay.create_user(params)
+
+    {:ok, user, %{email: params.email, password: params.password}}
+  end
+
   def authenticate(%{email: email, password: password}) do
     UserManager.authenticate(email, password)
     |> handle_sign_in()
